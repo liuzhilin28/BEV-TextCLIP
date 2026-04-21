33b723fe7a055e48b16ea3f72b7dc3c308d8df6d
liuzhilin28 <128899845+liuzhilin28@users.noreply.github.com>
2026-04-21 14:23:38 +0800

    Create _init_.py

---
+++ src/configs/_init_.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+说明: 配置文件包
+日期: 2026年1月22日
+"""
+
+from .bev_textclip_config import BEVTextCLIPConfig, get_config
+
+__all__ = ["BEVTextCLIPConfig", "get_config"]
